66fd7345c11864deb1a129597e12c311f68521aa
Bong'musa Dube <bongmusadube1@gmail.com>
2023-12-01 21:32:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchIcon from './search.svg';
+import { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
 
-function App() {
+// 81b2c873
+
+const API_URL = 'http://www.omdbapi.com?apikey=81b2c873';
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+
+   
+  const searchMovies =  async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+    console.log(movies)
+
+  };
+
+    useEffect(() => {
+      searchMovies('spiderman');
+
+
+    }, []);
+
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <h1>MoviesHub</h1>
+      <div className='search'>
+        
+        <input placeholder='Search for Movies' 
+        value={searchTerm} 
+        onChange={(e)=>{setSearchTerm(e.target.value);}}
+        /> 
+        <img src={SearchIcon} alt='' onClick={()=>{
+     
+            searchMovies(searchTerm);
+        }} />
+
+      </div>
+      {
+        movies.length > 0 ? (
+          <div className='container'> 
+           {
+            movies.map((movie) => {
+              return (
+              <MovieCard movie={movie}/>
+              )
+        
+            })
+          }
+    
+          </div>
+        ) : (
+          <div className='empty'>
+            <h3>There are no movies</h3>
+          </div>
+        )
+      }
+     
+
+   
+   </div>
   );
 }
 
